131b570351cb86527740578a2ef8c2fd24ae9141
Guilherme Nunes <guilherm3@gmail.com>
2023-02-15 15:45:45 -0300

    Revert "feature(*): implements detect device"

--- src/events/infrastructure/axios/headers/headers-generator.ts
+++ src/events/infrastructure/axios/headers/headers-generator.ts
@@ -1,22 +1,12 @@
 import { ORIGIN_URL } from "@/events/common/helpers/strings/constants";
 import { AxiosHeaders } from "axios";
 import { RequestParams } from "@/events/infrastructure/axios/requests/request-generator";
-import { Navigator } from 'node-navigator';
-const navigator = new Navigator();
 
 export function buildHeaders(params: RequestParams): AxiosHeaders {
-    const headers: AxiosHeaders = new AxiosHeaders()         
-    const devices: Record<string, any> = { 
-      app: navigator.userAgent.match(/Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i),
-      mobile: navigator.userAgent.match(/Chrome|Firefox|Safari|Opera|Edge/i),
-      desktop: navigator.platform.match(/darwin|win32|linux|freebsd|sunos|openbsd|aix/i)
-    }
-    const device = Object.keys(devices).find(device => devices[device]) || 'unknown';
-
-    
+    const headers: AxiosHeaders = new AxiosHeaders()
+  
     headers.set('Content-Type', 'application/json');
     headers.set('Origin', ORIGIN_URL);
-    headers.set('x-device-type', device);
   
     if (params.headerParams) {
       for (const [key, value] of Object.entries(params.headerParams)) {
@@ -28,7 +18,3 @@ export function buildHeaders(params: RequestParams): AxiosHeaders {
   
     return headers
   }
-
-  
-
-  // Exemplo NodeJS detectar dispositivo mobile ou desktop
