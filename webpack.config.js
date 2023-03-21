013ae0925cc9d0e35dc135ffea9ffc69b484f939
Guilherme Nunes <guilherm3@gmail.com>
2023-03-21 15:58:02 -0300

    WIP: configs webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-module.exports = {
+const npmConfig = {
   entry: './src/index.ts',
   module: {
     rules: [
@@ -27,6 +27,43 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     libraryTarget: 'umd',
-    path: path.resolve(__dirname, 'dist-es'),
+    path: path.resolve(__dirname, 'dist-esm'),
+  }
+};
+
+const browserConfig = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  optimization: {
+    minimize: true
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    modules: [
+        path.resolve(__dirname, 'node_modules'),
+        path.resolve(__dirname, './src'),
+    ],
+    alias: {
+      '@/events': path.resolve(__dirname, 'src', 'events'),
+    }
+  },
+  output: {
+    filename: 'bundle.js',
+    libraryTarget: 'commonjs2',
+    path: path.resolve(__dirname, 'dist-cjs'),
   },
 };
+
+
+module.exports = [
+  npmConfig,
+  browserConfig,
+];
