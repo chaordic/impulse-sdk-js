39296211235ba4670ac480d010943b8b7e6e4c35
Guilherme Nunes <guilherm3@gmail.com>
2023-03-23 14:37:32 -0300

    WIP: fix detect isBrowser

--- src/events/application/event-constructor.ts
+++ src/events/application/event-constructor.ts
@@ -1,9 +1,11 @@
+import { v4 as uuidv4 } from 'uuid';
+
+import { isBrowser } from "@/events/common/helpers/strings/isBrowser";
 import { API_KEY } from '@/events/common/helpers/strings/constants';
 import { getSystemInfo } from "@/events/common/helpers/strings/systemInfo";
 import { buildReferrerUrl } from "@/events/common/helpers/strings/buildReferrerUrl";
 import { buildDeviceType } from "@/events/common/helpers/strings/buildDeviceType";
 import { setCookie } from "@/events/common/helpers/strings/cookie";
-import { v4 as uuidv4 } from 'uuid';
 
 type Params<T> = {
     new (...args: any[]): T;
@@ -29,15 +31,18 @@ export class EventConstructor {
     setDefault() {
         this.pageData["default"] = { apiKey: API_KEY }
         this.pageData.default["page"] = this.pageName
-        this.pageData.default["info"] = {
-            referrer: buildReferrerUrl(),
-            pageViewId: setCookie('chaordic_browserId', UUID),
-            impulseSuiteCookie: setCookie('chaordic_browserId', UUID)
+
+        if (isBrowser()) {
+            this.pageData.default["info"] = {
+                referrer: buildReferrerUrl(),
+                pageViewId: setCookie('chaordic_browserId', UUID),
+                impulseSuiteCookie: setCookie('chaordic_browserId', UUID)
+            }
+            this.pageData.default["identity"] = {
+                session: setCookie('impulsesuite_session', new Date().getTime() + '-' + Math.random()),
+                browserId: setCookie('chaordic_browserId', UUID)
+            }
+            this.pageData.default["source"] = buildDeviceType(getSystemInfo())
         }
-        this.pageData.default["identity"] = {
-            session: setCookie('impulsesuite_session', new Date().getTime() + '-' + Math.random()),
-            browserId: setCookie('chaordic_browserId', UUID)
-        }
-        this.pageData.default["source"] = buildDeviceType(getSystemInfo())
     }
 }
