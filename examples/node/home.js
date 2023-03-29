bf2f74bead51afacf5125d478bdc34c9b6c742f0
Guilherme Nunes <guilherm3@gmail.com>
2023-03-29 15:17:10 -0300

    WIP: update example node

--- examples/node/home.js
+++ examples/node/home.js
@@ -5,13 +5,12 @@ const homeClass = linx.impulse.pages.home
 const home = new linx.impulse.events.constructor(homeClass)
 
 home.pageData
-    .source("desktop")
     .url('teste.com.br')
     .deviceId("fb4e49b6-35e3-42a1-a397-960f0b37ab6a")
     //... add more params builder
 
 const homeMergedData = { ...home.pageData.data, ...home.pageData.default };
 
-linx.impulse.events.homeView(homeMergedData).then((res) => console.log(k, res.status))
+linx.impulse.events.homeView(homeMergedData).then((res) => console.log(res.status))
 
 console.log(home, homeMergedData)
