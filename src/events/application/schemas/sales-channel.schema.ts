f469a570c58f9718dbf4927efbe937d06f70f242
Guilherme Nunes <guilherm3@gmail.com>
2023-06-01 10:28:04 -0300

    WIP: saleChannel string[]

--- src/events/application/schemas/sales-channel.schema.ts
+++ src/events/application/schemas/sales-channel.schema.ts
@@ -4,6 +4,14 @@ export const salesChannelSchema = z
     .string({
         invalid_type_error: "salesChannel must be a string"
     })
+    .or(
+        z.array(
+            z.string({
+                invalid_type_error: "salesChannel must be a array"
+            })
+            .nonempty(),
+        )
+    )
     .optional()
 
 export type SalesChannelInput = z.input<typeof salesChannelSchema>;
