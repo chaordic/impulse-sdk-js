bc278d5e8dc69736c90d832444426c5b7b73e449
Guilherme Garber <ggarber@protonmail.com>
2024-04-17 11:49:40 -0300

    [DIEVT-3189] Fix: change BASE_URL constant to string (#10)
    * fix: change constat for string
    
    * feat(doc): add PR template

--- src/events/common/helpers/strings/constants.ts
+++ src/events/common/helpers/strings/constants.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = new URL("https://api.event.linximpulse.net/v7/events/views")
+export const BASE_URL = 'https://api.event.linximpulse.net/v7/events/views'
 export const COOKIE_BROWSER_ID = 'chaordic_browserId'
 export const COOKIE_SESSION = 'impulsesuite_session'
 export const COOKIE_ANON_USER_ID = 'chaordic_anonymousUserId'
