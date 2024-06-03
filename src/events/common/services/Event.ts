23236268762cab946a9fdb01b6fe136298bc355a
ggarber42 <ggarber@protonmail.com>
2024-06-03 12:21:42 -0300

    Revert "fix: replace all URL type object for string (#11)"
    This reverts commit 0ab4a71ef4a6599d4fa1207c9daca67a32600863.

--- src/events/common/services/Event.ts
+++ src/events/common/services/Event.ts
@@ -134,9 +134,8 @@ export abstract class Event<T extends DefaultOutputValidation = DefaultOutputVal
     async send(eventData?: RequiredOnly<T> | OptionalsOnly<T>): Promise<any | Error> { 
         try {
             const parser = new ParserSchema(this.schema)
-            const url = `${BASE_URL}/${this.path}`
             const options = await new Request(
-                url,
+                new URL(`${BASE_URL}/${this.path}`),
                 'POST',
                 parser.validate({...this.data, ...eventData})
             )
